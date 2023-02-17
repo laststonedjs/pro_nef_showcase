7dbeb1683df2d1b3c72305911c6dd8543e8b37e3
Filip <filipsarovic428@gmail.com>
2023-02-17 20:31:17 +0100

    Finished the second section wrapper component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,20 @@ const App = () => {
   return (
     <>
       <SectionWrapper
-        title="You own store of Nifty NFTs. Start Selling & Growing."
+        title="You own store of Nifty NFTs. Start Selling & Growing"
         description="Buy, store, collect NFTs, exchange & earn crypto. 
         Join our community using ProNef Marketplace."
         showBtn
         mockupImg={assets.homeHero}
         banner="banner"
       />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a battery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
     </>
   );
 }
